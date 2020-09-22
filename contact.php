187fb619c08379610dda2370f5b36705f661e27e
nsCoder123 <goranivankovic87@gmail.com>
2020-09-22 11:21:29 +0200

    input

---
+++ contact.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio site</title>
+    <link rel="stylesheet" href="./contact.css">
+</head>
+<body>
+    <!--Div sa nav barom levo-->
+    
+    <div class="formPos">
+        
+        <form action="index.php" method="POST">
+            <h1>Contact</h1>
+           
+            <label for="Name">Name</label>
+                <input type="text" name="Name" required><br>
+            
+            <label for="Lname">Lastname </label>
+                <input type="text" name="Lname"required ><br>
+           
+            <label for="Email">Email</label>
+                <input type="email" name="Email" required ><br>
+                <input type="submit" value="submit" id="sub">
+               
+           
+          
+
+           
+
+
+        </form>
+
+
+
+    </div>
+
+    <div class="info">
+        <h1>gmail:  <strong>goranivankovic87@gmail.com</strong> </h1>
+        <h2>Telephone: <strong> +381/064-415-68-13 </strong></h2>
+
+
+    </div>
+
+
+
+
+   
+    <div class="gogaDesign">
+        <h3>gogaDesign</h3>
+    </div>
+
+
+    <button class="menuBar">
+        <i class="fa fa-bars menu" ></i>
+  
+        
+      </button>
+  
+  
+  <div class="div30"></div>
+
+
+    <script src="../js/contact.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+$name = $_POST['Name'];
+$Lname = $_POST['Lname'];
+$Email = $_POST['Email'];
+
+echo 'My name is '.$name.'<br>';
+echo 'My last name is '.$Lname.'<br>';
+echo 'My email is '.$Email.'<br>';
+
+
+
+
+?>
